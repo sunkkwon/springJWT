3069a8f259cec76341c62940630b8367046ed1c3
sunkk <sunkk@KWON>
2024-01-12 17:05:38 +0900

    20240112

---
+++ springJWT/src/main/java/com/study/spring/springjwt/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.study.spring.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@ResponseBody
+public class MainController {
+
+	@GetMapping("/")
+	public String mainP() {
+		return "Main Controller";
+	}
+	
+}
